4949d1dc29e6a3c4ba37ccaa285bab3e880f114f
Nachiket Kanore <nachiket.kanore@gmail.com>
2022-10-15 17:24:04 +0530

    added API endpoint for problem statement

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,56 @@
-use std::{process::{Command, Stdio}};
+use rocket::serde::json::Json;
+use serde::Serialize;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
 
-const DATASET_DIR: &str = "../dataset/";
+#[macro_use]
+extern crate rocket;
 
-fn main() {
+const DATASET_DIR: &str = "dataset/";
+
+#[get("/")]
+fn index() -> &'static str {
+    return "Welcome to code-assessments";
+}
+
+#[derive(Serialize)]
+struct Problem {
+    statement: String,
+    sample_inputs: Vec<String>,
+    sample_outputs: Vec<String>,
+}
+
+fn read_contents(file_path: &Path) -> String {
+    dbg!(file_path);
+    let file = File::open(file_path).expect("Unable to open file");
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).ok();
+    contents
+}
+
+#[get("/problem/<id>")]
+fn get(id: &str) -> Json<Problem> {
+    let stmt_path = Path::new(DATASET_DIR).join(id).join("stmt.txt");
+    let problem = Problem {
+        statement: read_contents(&stmt_path),
+        sample_inputs: Vec::new(),
+        sample_outputs: Vec::new(),
+    };
+    Json(problem)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, get])
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::process::Command;
 
     #[test]
     fn check_problems_exist() {
@@ -15,7 +58,10 @@ mod tests {
             .arg(DATASET_DIR)
             .output()
             .expect("Unable to read problem dataset directory");
-        let problem_count: usize = problem_list.stdout.split(|ch| u8::to_string(ch) == "\n").count();
+        let problem_count: usize = problem_list
+            .stdout
+            .split(|ch| u8::to_string(ch) == "\n")
+            .count();
         // println!("Problems found: {}", problem_count);
         assert!(problem_count > 0);
     }
